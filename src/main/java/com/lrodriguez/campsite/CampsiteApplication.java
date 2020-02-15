35f5cbb6c53e7815ecfefaa13ae1b3ad386412ee
Leandro Rodriguez <leandro.rodriguez@rappi.com>
2020-02-14 22:13:04 -0300

    Boilerplate code

---
+++ src/main/java/com/lrodriguez/campsite/CampsiteApplication.java
@@ -0,0 +1,15 @@
+package com.lrodriguez.campsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class CampsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampsiteApplication.class, args);
+    }
+
+}
--- src/main/java/com/lrodriguez/campsite/controllers/BookingController.java
+++ src/main/java/com/lrodriguez/campsite/controllers/BookingController.java
@@ -1,4 +1,10 @@
 package com.lrodriguez.campsite.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BookingController {
+
+
+
 }
